6eb393a3c0ea4eec36078e27b02fec0394d84103
Fergardi <fergardi@gmail.com>
2017-10-02 21:38:30 +0200

    Updated cloud function.

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,11 @@ const admin = require('firebase-admin')
 
 admin.initializeApp(functions.config().firebase)
 
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+exports.logger = functions.database.ref('users')
 .onWrite(event => {
-  // Grab the current value of what was written to the Realtime Database.
-  const original = event.data.val()
-  console.log('Uppercasing', event.params.pushId, original)
-  const uppercase = original.toUpperCase()
-  return event.data.ref.parent.child('uppercase').set(uppercase)
+  let log = {
+    name: 'cucu',
+    timestamp: Date.now()
+  }
+  return event.data.ref.parent.child('logs').push(log)
 })
