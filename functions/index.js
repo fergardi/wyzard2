ad9c3ce5bebb6f5264508b5ce148534739750a96
fergardi <fergardi@gmail.com>
2017-10-04 16:37:21 +0200

    Improved firebase functions.

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,25 @@ const admin = require('firebase-admin')
 
 admin.initializeApp(functions.config().firebase)
 
-exports.logger = functions.database.ref('users')
-.onWrite(event => {
-  return false
+exports.avarice = functions.database.ref('/users/{uid}/turns').onUpdate(event => {
+  admin.database().ref('users').child(event.params.uid).once('value', snapshot => {
+    console.log('User ' + snapshot.val().name + ' went from ' + event.data.previous.val() + ' turns to ' + event.data.current.val())
+  })
+  return true
+})
+
+// https://cron-job.org
+// firebase functions:config:set cron.key="passphrase"
+// https://us-central1-wyzard-14537.cloudfunctions.net/generosity?key=passphrase
+exports.generosity = functions.https.onRequest((req, res) => {
+  if (req.method === 'GET') {
+    admin.database().ref('users').once('value', snapshot => {
+      snapshot.forEach(user => {
+        user.ref.child('turns').set(Math.min(300, user.child('turns').val() + 1))
+      })
+    })
+    res.status(200).send()
+  } else {
+    res.status(404).send()
+  }
 })
